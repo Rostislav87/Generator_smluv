d81506779fac2a8e184e0af8bd644e43a969512c
Rostislav Rabiec <rosta.rabiec@icloud.com>
2022-11-20 17:47:57 +0100

    Creating a new program for generate contracts.

---
+++ generator_smluv.py
@@ -0,0 +1,3 @@
+Template_path = "C:\Users\rosta\Python\Python_kurz\Lekce 9\Generator_smluv\Templates"
+Contracts_path = "C:\Users\rosta\Python\Python_kurz\Lekce 9\Generator_smluv\Contracts"
+Employee_data = "C:\Users\rosta\Python\Python_kurz\Lekce 9\Generator_smluv"
