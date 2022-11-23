0997a094746c30b9dee6c66d4d00e91b59f71f9d
Rostislav Rabiec <rosta.rabiec@icloud.com>
2022-11-23 20:21:09 +0100

    Second commit

--- generator_smluv.py
+++ generator_smluv.py
@@ -1,3 +1,43 @@
-Template_path = "C:\Users\rosta\Python\Python_kurz\Lekce 9\Generator_smluv\Templates"
-Contracts_path = "C:\Users\rosta\Python\Python_kurz\Lekce 9\Generator_smluv\Contracts"
-Employee_data = "C:\Users\rosta\Python\Python_kurz\Lekce 9\Generator_smluv"
+# Zadat možnost vytvoření smlouvy
+def print_option():    
+    text = print("""
+Please select the option number of action you want to perform:
+0. salary change
+1. job change
+2. contract prolongation 
+""")
+    return text
+
+# Funkce pro nahrání vybrané šablony
+def load_template():
+    option = int(input(print_option()))
+
+    # Umístění šablon 
+    temp0 = "C:\\Users\\rosta\\Python\\Python_kurz\\Lekce 9\\Generator_smluv\\Templates\\salary_change.txt"  
+    temp1 = "C:\\Users\\rosta\\Python\\Python_kurz\\Lekce 9\\Generator_smluv\\Templates\\job_change.txt"
+    temp2 = "C:\\Users\\rosta\\Python\\Python_kurz\\Lekce 9\\Generator_smluv\\Templates\\contract_prolongation.txt" 
+    if option == 0:
+        with open(temp0) as template:
+            return template.read()
+    elif option == 1:
+         with open(temp1) as template:
+            return template.read()
+    else:
+        with open(temp2) as template:
+            return template.read()        
+    
+        
+# Funkce pro nahrání zaměstnaneckých dat
+def load_employees_data():
+
+    # Umístění zaměstnaneckých dat
+    employee_data_path = "C:\\Users\\rosta\\Python\\Python_kurz\\Lekce 9\\Generator_smluv\\employees.txt"
+    with open (employee_data_path) as employee_file:
+        return employee_file.read()
+
+# Umístění pro vygenerované smlouvy
+contracts = "C:\\Users\\rosta\\Python\\Python_kurz\\Lekce 9\\Generator_smluv\\Contracts"
+
+load_template()
+load_employees_data()
+
